2ffbd911736cdfb7c0b25ebf7620b05f1cf57e5d
Jean <jeanluca.fernandes@totvs.com.br>
2021-02-23 08:25:01 -0300

    finalizado modulo-9

---
+++ iniciando-jpa/src/main/java/com/curso/dto/ProdutoDto.java
@@ -0,0 +1,13 @@
+package com.curso.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProdutoDto {
+
+    private Integer id;
+
+    private String nome;
+}
